c0b318ee7cd64bd3980dd06a7155d235a3cf2521
kojesung <js95112345@gmail.com>
2026-01-25 15:54:36 +0900

    feat: tailwind 설치 및 색상 및 폰트 토큰 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <>test</>;
+  return (
+    <div className="container-custom py-12">
+      <h1 className="heading-xlarge text-primary-600">제목</h1>
+      <p className="body-large text-gray-700">본문</p>
+    </div>
+  );
 }
 
 export default App;
