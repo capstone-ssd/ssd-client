c98ac1cf32f5caa6bb69333c1ed7b94d443f4b8a
kojesung <js95112345@gmail.com>
2026-02-03 03:38:23 +0900

    feat: onClick이 있는 div -> button 태그로 변경하여 접근성 개선

--- src/components/markdown/BlockWrapper.tsx
+++ src/components/markdown/BlockWrapper.tsx
@@ -24,10 +24,11 @@ export default function BlockWrapper({
     openSidebar('comments', blockId);
   };
   return (
-    <div
+    <button
       data-block-id={blockId}
       onClick={handleClick}
       className={cn(
+        'w-full text-left',
         'relative cursor-pointer rounded-lg transition-all duration-200',
         'mb-4 py-2 pl-4',
         isSelected && 'bg-blue-100',
@@ -37,6 +38,6 @@ export default function BlockWrapper({
       )}
     >
       {children}
-    </div>
+    </button>
   );
 }
