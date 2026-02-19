25c5a781a3bdf346da75f69fd9c0f794fdbeeaf7
kojesung <js95112345@gmail.com>
2026-02-20 01:51:37 +0900

    feat: ProfileSection 타이포 적용

--- src/components/accordion/content/AccordionProfileSection.tsx
+++ src/components/accordion/content/AccordionProfileSection.tsx
@@ -21,15 +21,15 @@ const AccordionProfileSection = ({
   children,
 }: AccordionProfileSectionProps) => (
   <section className="flex gap-5">
-    <div className="flex w-[38px] shrink-0 flex-col items-center">
+    <div className="flex h-[65px] w-[38px] shrink-0 flex-col items-center justify-center">
       <SvgProfile width={38} height={38} />
     </div>
 
-    <div className="flex flex-1 flex-col gap-5">
+    <div className="flex flex-1 flex-col gap-2.5">
       <div className="flex flex-col gap-0.5">
-        <span className="text-[18px] leading-normal font-medium text-gray-800">{userName}</span>
-        <span className="text-[16px] leading-normal font-normal text-gray-400">{userEmail}</span>
-        <span className="text-[14px] leading-normal font-normal text-gray-400">{timestamp}</span>
+        <span className="text-xsmall text-gray-800">{userName}</span>
+        <span className="text-xxsmall text-gray-400">{userEmail}</span>
+        <span className="text-tiny text-gray-400">{timestamp}</span>
       </div>
       {children}
     </div>
